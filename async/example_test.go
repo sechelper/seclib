e45df550dd7e6666c76e0aa81dc24cfa4db36cfa
cookun <cookun@sechelper.com>
2023-01-30 20:00:27 +0800

    update Goroutine

--- async/example_test.go
+++ async/example_test.go
@@ -3,26 +3,19 @@ package async_test
 import (
 	"fmt"
 	"github.com/sechelper/seclib/async"
-	"sync"
 )
 
 func ExampleGoroutine() {
-	wg := sync.WaitGroup{}
-	wg.Add(100000)
-
-	c := make(chan any, 10)
-	input := func(...any) {
+	input := func(c *chan any) {
 		for i := 0; i <= 100000; i++ {
-			c <- i
+			*c <- i
 		}
-
 	}
+
 	process := func(p ...any) {
 		fmt.Println(p[0])
-		wg.Done()
 	}
 
-	async.Goroutine(30, &c, input, process)
+	async.Goroutine(30, input, process)
 
-	wg.Wait()
 }
--- async/goroutine.go
+++ async/goroutine.go
@@ -3,28 +3,28 @@ package async
 import "time"
 
 // Goroutine asynchronous processing of data in chan
-func Goroutine(n int, c *chan any, input func(...any), process func(...any)) {
+func Goroutine(n int, input func(*chan any), process func(...any)) {
 	done := make(chan struct{})
+	c := make(chan any, n)
 
 	for i := 0; i < n; i++ {
 		go func() {
 			for {
 				select {
-				case args := <-*c:
+				case args := <-c:
 					process(args)
 				case <-done:
 					return
-				default:
 				}
 			}
 		}()
 	}
 
-	input(c)
+	input(&c)
 
 	// clear chan
 	for {
-		if len(*c) == 0 {
+		if len(c) == 0 {
 			close(done)
 			return
 		}
