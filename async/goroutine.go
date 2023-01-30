942a0a89959ed69100eddabb6abf185d3bc059ad
cookun <cookun@sechelper.com>
2023-01-30 21:31:49 +0800

    update Goroutine

--- async/goroutine.go
+++ async/goroutine.go
@@ -1,33 +1,36 @@
 package async
 
-import "time"
+import (
+	"sync"
+)
 
 // Goroutine asynchronous processing of data in chan
 func Goroutine(n int, input func(*chan any), process func(...any)) {
 	done := make(chan struct{})
 	c := make(chan any, n)
+	wg := sync.WaitGroup{}
+	wg.Add(n)
 
 	for i := 0; i < n; i++ {
 		go func() {
+			defer wg.Done()
 			for {
 				select {
 				case args := <-c:
+					if args == nil {
+						return
+					}
 					process(args)
 				case <-done:
 					return
 				}
 			}
+
 		}()
+
 	}
 
 	input(&c)
-
-	// clear chan
-	for {
-		if len(c) == 0 {
-			close(done)
-			return
-		}
-		time.Sleep(500)
-	}
+	close(c)
+	wg.Wait()
 }
